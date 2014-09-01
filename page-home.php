358db8bced19691184830ab17dcec57955551914
Tim Rodgers <timbocf@gmail.com>
2014-09-01 15:23:52 -0700

    Changed root dir
    Changed root dir from "themes/three-sixty" to "repos/360-Fulfillment"

---
+++ page-home.php
@@ -0,0 +1,61 @@
+<?php get_header(); ?>
+
+<div class="content-wrap">
+
+	<div class="container">
+
+		<div class="col-md-6 col-md-push-6 boxes">
+
+			<img src="http://360fulfillmentnow.com/wp-content/themes/three-sixty/images/sliding-boxes.png">
+
+		</div>
+
+		<div class="col-md-6 col-md-pull-6 home-caption">
+
+			<p>360 Fulfillment is a total turn-key, marketing-mail fulfillment company that offers personalized service.  No job is too large or too small for us to assist you with.  We stand behind our service and pride ourselves on customer satisfaction.</p>
+
+		</div>
+
+	</div>
+
+</div>
+
+<!-- <div class="line"></div> -->
+
+<div class="row service-row">
+
+	<div class="col-md-4">
+
+		<h3>Direct Mail Fulfillment</h3>
+
+	</div>
+
+	<div class="col-md-4">
+
+		<h3>Lumpy Mail Fulfillment</h3>
+
+	</div>
+
+	<div class="col-md-4">
+
+		<h3>Personalized Service</h3>
+
+	</div>
+
+</div>
+
+<div class="home-service-list">
+
+	<ul>
+
+		<li>Total Fulfillment of all marketing projects</li>
+
+		<li>Personalized service includes: hand signing and hand addressing any marketing projects</li>
+
+		<li>Competitive Pricing</li>
+
+	</ul>
+
+</div>
+
+<?php get_footer(); ?>
